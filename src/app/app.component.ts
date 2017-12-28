378505b39b99249a5e16ccdee1fc378b96de8d33
Prasanth NV <prasanthcodemagos@gmail.com>
2017-12-28 11:30:21 +0530

    template driven from implemented

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
+import { OnInit } from '@angular/core/src/metadata/lifecycle_hooks';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+
   title = 'app';
+  contactList:any[] = [];
+
+  ngOnInit(){
+   this.getData();
+  }
+  onSubmit(form){
+    let value = form.value;
+    console.log(value);
+    this.contactList.push(value);
+    localStorage.setItem("contact_list",JSON.stringify(this.contactList));
+  }
+
+
+  getData(){
+    if(localStorage.getItem("contact_list")){
+      this.contactList = JSON.parse(localStorage.getItem("contact_list"));
+    }
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
